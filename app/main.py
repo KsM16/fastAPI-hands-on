adf75321f2df7b143fa7f8b71b04f1ed56dce168
Kshitiz Manral <kshitizmanral@gmail.com>
2026-06-26 23:12:11 +0530

    added Delete and Update endpoints

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
-from sqlalchemy import select
+from sqlalchemy import delete, select, update
 from sqlalchemy.orm import Session
 
 from .database import engine, Base, get_db
@@ -18,9 +18,14 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+    title: str               
+    content: str                
+    published: bool = True      
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = None         
+    content: Optional[str] = None     
+    published: Optional[bool] = None
 
 try:
     conn = psycopg2.connect(
@@ -94,6 +99,50 @@ async def get_post(id: int, db: Session = Depends(get_db)):
     return {"post_details": result}
 
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    statement = delete(models.Post).where(models.Post.id == id)
+    result = db.execute(statement)
+    
+    if result.rowcount == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with ID {id} does not exist"
+        )
+    
+    db.commit()
+    return None
+
+
+@app.patch("/posts/{id}", status_code=status.HTTP_200_OK, response_model = Post)
+async def update_post(id: int, updated_post: PostUpdate, db: Session = Depends(get_db)):
+    
+  
+    update_data = updated_post.model_dump(exclude_unset=True)
+    
+    if not update_data:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="At least one field must be provided for update"
+        )
+
+    statement = (
+        update(models.Post)
+        .where(models.Post.id == id)
+        .values(**update_data)
+        .returning(models.Post)
+    )
+    result = db.execute(statement)
+    updated_record = result.scalar_one_or_none()
+    
+    if not updated_record:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with ID {id} does not exist"
+        )
+    
+    db.commit()
+    return  updated_record
 
 # @app.get("/posts/{id}")
 # async def get_post(id: int):
